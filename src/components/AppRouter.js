f5ccd9aabf14f43c6299bdb6ba582b90c7e7b467
nigberg <nigberg@gmail.com>
2023-04-11 00:19:01 +0300

    user store created

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,12 +2,15 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
 import { SHOP_ROUTE } from '../utils/constants';
 import Shop from '../pages/Shop';
+import { Context } from '../index';
+import { useContext } from 'react';
 
 const AppRouter = () => {
-  let isAuth = false;
+  const {user} = useContext(Context);
+  console.log(user)
   return(
     <Routes>
-      {isAuth && authRoutes.map(route =>
+      {user.isAuth && authRoutes.map(route =>
       <Route key={route.path} path={route.path} element={<route.component/>} exact />
       )}
       {publicRoutes.map(route =>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import UserStore from './store/UserStore';
+
+export const Context = createContext(null);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Context.Provider value={{user: new UserStore()
+    }}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Context.Provider>
   </React.StrictMode>
 );
 
