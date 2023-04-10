797377129d641bc4b5185946027f9f53913e3eb7
nigberg <nigberg@gmail.com>
2023-04-10 23:56:37 +0300

    app routes created

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,10 +1,20 @@
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/constants';
+import Shop from '../pages/Shop';
 
 const AppRouter = () => {
+  let isAuth = false;
   return(
-    <>
-      App router
-    </>
+    <Routes>
+      {isAuth && authRoutes.map(route =>
+      <Route key={route.path} path={route.path} element={<route.component/>} exact />
+      )}
+      {publicRoutes.map(route =>
+      <Route key={route.path} path={route.path} element={<route.component/>} exact />
+      )}
+      <Route path='*' element={<Shop/>}/>
+    </Routes>
   )
 
 };
